c75b05249a6558e980b1a4a4e1f6d7d3896842cb
mdarifmia <arif@supremesupports.com.au>
2024-01-06 12:39:07 +0600

    invokable controller or single action controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use App\Http\Controllers\TeacherController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -146,4 +147,7 @@ Route::get('user/{id}', [UserController::class, 'index']);
 
 
 Route::resource('teacher', TeacherController::class);
+
+
+Route::get('blog', BlogController::class);
 
