92c87f698d480b902e4e5826b0b017b3ffb0d5eb
mdarifmia <arif@supremesupports.com.au>
2024-01-12 10:16:49 +0600

    show error message under the text input and make border red

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -21,5 +21,7 @@ class ContactController extends Controller
                 'message'=>'required'
             ]
         );
+
+        dd($request->all());
     }
 }
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -10,36 +10,68 @@
         .mb_10{
             margin-bottom: 10px;
         }
+        .error{
+            color:red;
+        }
+        .input-error{
+            border:1px solid red;
+        }
     </style>
 
 </head>
 <body>
     <h2>Write contact information with bellow form</h2>
 
-    @if ($errors->any())
+    {{-- @if ($errors->any())
         @foreach ($errors->all() as $error)
             {{ $error }} <br>
         @endforeach
-    @endif
+    @endif --}}
 
     <form action="{{ route('contact_store') }}" method="post">
         @csrf
         <div>
             <div class="mb_10">
                 <label for="">Name</label> <br>
-                <input type="text" name="name" id="name" placeholder="Write your name here" value="{{ old('name') }}">
+                <input type="text" name="name" id="name" placeholder="Write your name here" value="{{ old('name') }}" class="@error('name')
+                    input-error
+                @enderror">
+                <br>
+                @error('name')
+                   <small class="error"> {{ $message }}</small>
+                @enderror
             </div>
             <div class="mb_10">
                 <label for="">Phone</label> <br>
-                <input type="text" name="phone" id="phone" placeholder="Write your phone here" value="{{ old('phone') }}">
+                <input type="text" name="phone" id="phone" placeholder="Write your phone here" value="{{ old('phone') }}" class="@error('phone')
+                    input-error
+                @enderror">
+                <br>
+                @error('phone')
+                    <small class="error">{{ $message }}</small>
+                @enderror
             </div>
             <div class="mb_10">
                 <label for="">Email</label> <br>
-                <input type="text" name="email" id="email" placeholder="Write your email here" value="{{ old('email') }}">
+                <input type="text" name="email" id="email" placeholder="Write your email here" value="{{ old('email') }}" class="@error('email')
+                    input-error
+                @enderror">
+                <br>
+                @error('email')
+                    <small class="error">{{ $message }}</small>
+                @enderror
             </div>
             <div class="mb_10">
-                <label for="">Email</label> <br>
-                <textarea name="message" id="message" placeholder="Write your message here" cols="30" rows="5">{{ old('message') }}</textarea>
+                <label for="">Message</label> <br>
+                <textarea name="message" id="message" placeholder="Write your message here" cols="30" rows="5" class="@error('message')
+                 input-error
+                @enderror">{{ old('message') }}</textarea>
+                <br>
+                @error('message')
+
+                <small class="error">{{ $message }}</small>
+
+                @enderror
             </div>
             <button type="submit">Submit</button>
         </div>
