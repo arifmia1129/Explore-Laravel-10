28b81149c4795a62577dcd351515abdc0babf66e
mdarifmia <arif@supremesupports.com.au>
2024-01-13 11:04:56 +0600

    file upload to public folder

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,4 +9,14 @@ class HomeController extends Controller
     public function index() {
         return view('home');
     }
+
+    public function store(Request $request){
+        // Assuming 'my_file' is the name attribute of your file input in the form
+        $final_name = 'new_file.'.$request->file('my_file')->extension();
+
+        $request->my_file->move(public_path('uploads'), $final_name);
+
+        echo $final_name;
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AdmissionController;
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\TeacherController;
 use App\Http\Controllers\UserController;
@@ -21,9 +22,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::post('store', [HomeController::class, 'store'])->name('store');
 
 Route::get('welcome', function(){
     return view('welcome');
